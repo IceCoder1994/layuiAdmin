b19d6b5a23ecb84ce5d44e9f6c90f4894ecd4af9
ice <1034939359@qq.com>
2019-06-28 18:32:22 +0800

    first commit

--- component/multi_image_upload.js
+++ component/multi_image_upload.js
@@ -1,44 +1,71 @@
 var uploadDialogHtml = `
 <div class="upload-image-list">
-        <input type="file" id="ice-multi-upload-file" style="display: none;" onchange="multiImageUpload.onFileUpload()">
-        <div class="addDiv" onclick="javascript:$('#ice-multi-upload-file').click()">
+        <input type="file" class="ice-multi-upload-file" style="display: none;">
+        <div class="addDiv">
         <img src="../image/add.png">
     </div>
     </div>`;
 
-var multiImageUpload = {
+var MultiImageUpload = {
     files: [],
     selector:'',
     init: function (selector) {
+        var context=this;
         this.selector=selector;
         $(selector).css("display", 'none');
         $(selector).after(uploadDialogHtml);
+        $(selector).next().find('.ice-multi-upload-file').change(function () {
+            context.onFileUpload();
+        });
+        $(selector).next().find('.addDiv').click(function () {
+            $(this).siblings('.ice-multi-upload-file').click();
+        });
         return this;
     },
     deleteImg: function (index) {
+        var context=this;
         this.files.splice(index, 1);
+        console.log(index);
         $(this.selector).val(this.files);
-        $(`.upload-image-list .item`)[index].className = "delItem";
+        // $(`.upload-image-list .item`)[index].className = "delItem";
+         $(this.selector).next().children('.item')[index].className = "delItem";
+        $(this.selector).next().find('.item .delete').each(function (index,item) {
+            $(item).unbind('click').click(function () {
+                context.deleteImg(index)
+            });
+        });
     },
     onFileUpload: function () {
-        this.files.push($('#ice-multi-upload-file')[0].files[0]);
+        // this.files.push($('#ice-multi-upload-file')[0].files[0]);
+        this.files.push($(this.selector).next().find('.ice-multi-upload-file')[0].files[0]);
         $(this.selector).val(this.files);
-        console.log($(this.selector));
         var reads = new FileReader();
-        reads.readAsDataURL($('#ice-multi-upload-file')[0].files[0]);
-        $('#ice-multi-upload-file').val('');
+        console.log($(this.selector).next().find('.ice-multi-upload-file')[0].files);
+        reads.readAsDataURL($(this.selector).next().find('.ice-multi-upload-file')[0].files[0]);
+        $(this.selector).next().find('.ice-multi-upload-file').val('');
+        var context=this;
         reads.onload = function () {
-            if ($('.upload-image-list .item:last').length > 0) {
-                $('.upload-image-list .item:last').after(`<div class="item">
+            if ( $(context.selector).next().children('.item:last').length > 0) {
+                $(context.selector).next().children('.item:last').after(`<div class="item">
                     <img class="image" src="${this.result}"/>
-                    <img src="../image/delete.png" class="delete" onclick="multiImageUpload.deleteImg(multiImageUpload.files.length-1)">
+                    <img src="../image/delete.png" class="delete">
                 </div>`);
             } else {
-                $('.upload-image-list .addDiv').before(`<div class="item">
+                $(context.selector).next().children('.addDiv').before(`<div class="item">
                     <img class="image" src="${this.result}"/>
-                    <img src="../image/delete.png" class="delete" onclick="multiImageUpload.deleteImg(multiImageUpload.files.length-1)">
+                    <img src="../image/delete.png" class="delete">
                 </div>`);
             }
+
+            // $(context.selector).next().find('.delete').unbind('click').click(function () {
+            //     context.deleteImg(context.files.length-1)
+            // })
+            console.log($(context.selector).next().find('.item .delete'));
+            $(context.selector).next().find('.item .delete').each(function (index,item) {
+                $(item).unbind('click').click(function () {
+                    context.deleteImg(index)
+                });
+            });
         };
     },
 };
